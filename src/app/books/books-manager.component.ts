384e88e6da4b2c8822401c3a7e29a707393a5b31
dileepguptha31 <dileepguptha31@gmail.com>
2022-07-19 10:37:59 +0530

    1 file modified, 1 added

--- src/app/books/books-manager.component.ts
+++ src/app/books/books-manager.component.ts
@@ -2,7 +2,29 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'books-manager',
-  template: 'Books Manager',
+  templateUrl: 'books-manager.component.html',
   styles: [''],
 })
-export class BooksManagementComponent {}
+export class BooksManagementComponent {
+  public data = '';
+
+  // Instance Data Member
+  public books = [
+    { title: 'T1', author: 'A1', price: 400 },
+    { title: 'T2', author: 'A2', price: 405 },
+    { title: 'T3', author: 'A3', price: 405 },
+    { title: 'T4', author: 'A4', price: 405 },
+  ];
+
+  public deleteBook(book: any) {
+    this.books.splice(this.books.indexOf(book), 1);
+  }
+
+  public addNewBook() {
+    this.books.push({ title: '', author: '', price: 0 });
+  }
+
+  public display() {
+    this.data = JSON.stringify(this.books);
+  }
+}
