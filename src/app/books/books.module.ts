b3047470f86ddeb0faa445009465b66848e228c7
dileepguptha31 <dileepguptha31@gmail.com>
2022-07-19 10:27:28 +0530

    3 files modified

--- src/app/books/books.module.ts
+++ src/app/books/books.module.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BooksManagementComponent } from './books-manager.component';
 
 @NgModule({
-  imports: [FormsModule],
+  imports: [FormsModule, CommonModule],
   declarations: [BooksManagementComponent],
   providers: [],
 })
--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { CounterComponent } from './counter.component';
 import { ProductComponent } from './product.component';
 import { ProductsManagerComponent } from './products-manager.component';
 
 @NgModule({
-  imports: [],
+  imports: [CommonModule],
   declarations: [ProductsManagerComponent, ProductComponent, CounterComponent],
   providers: [],
 })
