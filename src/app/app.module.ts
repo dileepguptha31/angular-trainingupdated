84582e656dff55c6fc624320bed1ebf9afafaeaf
dileepguptha31 <dileepguptha31@gmail.com>
2022-07-19 09:41:08 +0530

    1 file modified, 3 added, 3 deleted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { ProductComponent } from './product/productComponent';
+import { ProductComponent } from './product/product.component';
 import { CounterComponent } from './counterComponent';
-import { ProductsManagerComponent } from './product/ProductsManagerComponent';
+import { ProductsManagerComponent } from './product/products-manager.component';
 import { HelloComponent } from './hello.component';
 import { RouterModule } from '@angular/router';
 import { BooksManagementComponent } from './books/booksmanagercomponent';
